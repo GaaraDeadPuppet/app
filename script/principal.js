fc7eb81556618e79528486370f18a02c11631592
GaaraDeadPuppet <ikani_93@hotmail.com>
2016-08-31 19:48:29 +0200

    json froga

--- script/principal.js
+++ script/principal.js
@@ -7,10 +7,26 @@ $("#p_a").click(cargar_proxima_actividad);
 
 
 function cargar_proxima_actividad(){
+/*
 var url;
 url="http://www.ilazkitaldea.com/app/php/cargar_proxima_actividad.php";
 url=encodeURI(url);
 $('#menu').load(url,asignar_menu);
+*/
+var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+    });
 }
 
 function asignar_menu(){
