e5584d1c4088c37dacd0160e0c1a7b494fe086d5
GaaraDeadPuppet <ikani_93@hotmail.com>
2016-08-31 19:38:50 +0200

    header

--- script/principal.js
+++ script/principal.js
@@ -1,22 +1,17 @@
-/*
+
 function iniciar(){
 console.log("inicio");
-//$("#p_a").click(cargar_proxima_actividad);
-}
-*/
-function iniciar(){
-alert("Hello! I am an alert box!!");
+$("#p_a").click(cargar_proxima_actividad);
 }
 
-/*
+
+
 function cargar_proxima_actividad(){
 var url;
-url="./php/cargar_proxima_actividad.php";
+url="http://www.ilazkitaldea.com/app/php/cargar_proxima_actividad.php";
 url=encodeURI(url);
-alert("aaa");
 $('#menu').load(url,asignar_menu);
-alert("aaa");
 }
 
 function asignar_menu(){
-}*/
+}
